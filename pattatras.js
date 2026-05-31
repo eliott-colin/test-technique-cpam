9ac9cb53d837be62e7dae1e0b643d1a10b9cd10c
elicoptere <161453411+eliott-colin@users.noreply.github.com>
2026-05-31 19:36:02 +0200

    feat : implement Pattatras ui small change on pattatras.js add function and calling button

--- pattatras.js
+++ pattatras.js
@@ -1,12 +1,24 @@
-
-for (let i = 1; i < 6457; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("Pattatras");
-    } else if (i % 3 === 0) {
-        console.log("Patte");
-    } else if (i % 5 === 0) {
-        console.log("Tatras");
-    }else {
-        console.log(i);
+function pattatras(n) {
+    let res = "";
+    for (let i = 1; i < n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            res += i + ": Pattatras<br>";
+        } else if (i % 3 === 0) {
+            res += i + ": Patte<br>";
+        } else if (i % 5 === 0) {
+            res += i + ": Tatras<br>";
+        }else {
+            res += i + "<br>";
+        }
     }
+    return res;
 }
+
+const startButton = document.getElementById("startButton");
+const resultDiv = document.getElementById("result");
+
+startButton.addEventListener("click", () => {
+    let result = "";
+    result += pattatras(6457);
+    resultDiv.innerHTML = result;
+});
